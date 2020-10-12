0cf446167ef4fe3725a30f2595649acec32c1f14
Sanuja <a.rajmal@outlook.com>
2020-10-12 13:19:47 +0530

    innitial commit

---
+++ VirtualClass/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("Get")]
+        public IActionResult GetValues() 
+        {
+            return  Ok( new { value = "Values Ok" });
+        }
+
+
+        [Authorize]
+        [HttpGet("Secret")]
+        public IActionResult GetSecretValues()
+        {
+            return Ok(new { value = "Secret Values Ok" });
+        }
+    }
+}
---
+++ VirtualClass/Users/AuthenticationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualClass.Users
+{
+    public class AuthenticationResponse
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
