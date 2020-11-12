28ec21f0794271ad06e6e6eef5a58891860081ef
Sanuja <a.rajmal@outlook.com>
2020-11-12 22:01:11 +0530

    New classes added to DbContext

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -16,5 +16,17 @@ namespace Entities
         public DbSet<Owner> Owners { get; set; }
         public DbSet<Account> Account { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Chapter> Chapter { get; set; }
+        public DbSet<ContactNumber> ContactNumber { get; set; }
+        public DbSet<MaterialType> MaterialType { get; set; }
+        public DbSet<Package> Package { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<StudyMaterial> StudyMaterial { get; set; }
+        public DbSet<Subject> Subject { get; set; }
+        public DbSet<SubjectCatogary> SubjectCatogary { get; set; }
+        public DbSet<UserVirtualClass> UserVirtualClass { get; set; }
+        public DbSet<VitualClass> VitualClass { get; set; }
+
     }
 }
